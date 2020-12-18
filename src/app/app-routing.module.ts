2204f8bfe016f2e6e60d9fe7789b2bc1ab2d4b0b
Sergio Martin <sergiomp98@gmail.com>
2020-12-18 12:55:27 +0100

    18/12/20

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PaginaPrincipalComponent } from './front/pagina-principal/pagina-principal.component';
 import { PaginaCursosComponent } from './front/pagina-cursos/pagina-cursos.component';
+import { ContactFotterComponent } from './contact-fotter/contact-fotter.component';
 
 const routes: Routes = [
   {path: '', component: PaginaPrincipalComponent},
   // {path: 'inicio', component: PaginaPrincipalComponent},
-  {path: 'cursos', component: PaginaCursosComponent}
+  {path: 'cursos', component: PaginaCursosComponent},
+  {path: 'contacto', component: ContactFotterComponent}
 ];
 
 @NgModule({
